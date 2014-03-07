fed437ad385e6df41c99d55719c978d65dccaa2b
Lu Yao <yaolu1103@gmail.com>
2014-03-08 00:01:54 +0800

    update code

--- Makefile
+++ Makefile
@@ -1,58 +1,23 @@
-ifeq ($(shell idl/genIf.sh ads idl),$(PWD))
-endif
 CC=g++
-SHELL=/bin/sh
-CURRENT_PATH=$(PWD)
-INTERFACE_PATH=$(CURRENT_PATH)/idl/ads
 
-INTERFACE_OBJ:=$(patsubst %.cpp,%.o, $(wildcard ${INTERFACE_PATH}/src/*.cpp))
-
-INCLUDES=-I./include -I$(INTERFACE_PATH)/include -I./hashtable -I/usr/local/include/thrift/
-LIB_PATH=-L. -L./lib -L/usr/local/lib/
+INCLUDES=-I./include
+LIB_PATH=-L. -L./lib
 
 CPPFLAGS=-O0 -g -static -fPIC -finline-functions -Winline -Werror -pipe \
         -Wreturn-type -Wtrigraphs -Wformat -Wparentheses -Wpointer-arith -Wall \
         --param large-function-growth=10000 --param inline-unit-growth=1000 \
         -D_XOPEN_SOURE=500 -D_GNU_SOURCE #-DTEST
 
-LDFLAGS= -lm -lcrypt -lpthread -lgflags -lserv -lcap -llog \
-		 -lini_config -lconn -lcluster -lzookeeper_mt \
-		 -lthrift -lz
+LDFLAGS=""
 
-all: ads client_py_code 
-
-test: testcmd ads_agent_test ads_cfg_test
+all: reflect
 
 %.o: %.cpp 
 	$(CC) $(CPPFLAGS) $(INCLUDES) -c $< -o $@ 
 
-ads: src/main.o src/ads_conf.o src/ads_conn.o src/ads_db_proc.o src/ads_init.o\
-	src/ads_agent.o src/ads_agent_factory.o src/ads_agent_cfg.o src/db_layer_mgr.o\
-	$(INTERFACE_OBJ)\
-	src/ads_db_strategy.o src/hi_redis.o src/anet.o src/sds.o
-	$(CC) -o $@ $^ $(INCLUDES) $(LIB_PATH) $(LDFLAGS) 
-
-testcmd: test/redisTest.o\
-	src/hi_redis.o src/anet.o src/sds.o
+reflect: test/reflect.o test/class_define.o
 	$(CC) -o $@ $^ $(INCLUDES) $(LIB_PATH)
-
-testclient:	test/client.o 
-	$(CC) -o $@ $^ $(INCLUDES) $(LIB_PATH)
-
-ads_agent_test: test/ads_agent_test.o \
-	src/ads_agent.o src/ads_agent_factory.o
-	$(CC) -o $@ $^ $(INCLUDES) $(LIB_PATH) $(LDFLAGS) 
-
-ads_cfg_test: test/ads_cfg_test.o \
-	src/ads_agent_cfg.o 
-	$(CC) -o $@ $^ $(INCLUDES) $(LIB_PATH) $(LDFLAGS)
-
-client_py_code:
-	sh ./idl/gen.sh py
-	/bin/rm -rf test/ads
-	mv idl/gen-py/ads test/
 
 clean:
-	rm -rf src/*.o test/*.o ads ./test/ads ./idl/ads
-	rm -rf testcmd
+	rm -rf src/*.o test/*.o reflect
 
--- include/class_factory.h
+++ include/class_factory.h
@@ -6,77 +6,87 @@
 
 typedef void *(*CreateFuntion) (void);
 
+typedef std::map < std::string, CreateFuntion > ClassFactoryDict;
+typedef ClassFactoryDict::const_iterator ClassFactoryDictCIter;
+
+static ClassFactoryDict& _getMap();
+
+#define DEFINE_CLASS(class_name) \
+	const char class_name##Argv[] = #class_name;\
+	class class_name: public Register<class_name, class_name##Argv>
+
+//#define DEFINE_BASE_CLASS(class_name)
+
+#define DEFINE_CLASS_EX(class_name, father_class)\
+	const char class_name##Argv[] = #class_name;\
+	class class_name:public Register<class_name,class_name##Argv>, public father_class
+
 class ClassFactory{
 public:
-	typedef std::map < std::string, CreateFuntion > ClassFactoryDict;
-	typedef std::map < std::string, CreateFuntion >::const_iterator ClassFactoryDictCIter;
-	static void *GetClass(const std::string & name);
+	static void *GetClass(const std::string & name){
+		ClassFactoryDictCIter it_find = _getMap().find(name);
+		if (it_find == _getMap().end())return NULL;
+		else return it_find->second();
+	}
+
 	static void RegistClass(const std::string &name, CreateFuntion method){
 		_getMap().insert(ClassFactoryDict::value_type(name, method));
 	}
 private:
 	ClassFactory();
 	ClassFactory(const ClassFactory&);
-	ClassFactory& operator(const ClassFactory&);
-
-	std::map< std::string, CreateFuntion >& _getMap(){
-		static  ClassFactoryDict _map;
-		return  _map;
-	}
+	ClassFactory& operator=(const ClassFactory&);
 };
 
-void* ClassFactory::GetClass(const std::string & name)
-{
-	ClassFactoryDictCIter it_find = _getMap().find(name);
-	if (it_find == _getMap().end()) {
-		return NULL;
-	} else {
-		return it_find->second();
-	}
-}
 
 class RegistyClass {
-using std::string;
 public:
-	RegistyClass(const string & name, CreateFuntion method) {
+	RegistyClass(const std::string & name, CreateFuntion method) {
 		ClassFactory::RegistClass(name, method);
 	}
+	void init()const{}
 };
 
-template < class T, const char name[] > 
+enum CLASS_TYPE{
+	DEFAULT = 0,
+	SINGLETON,
+	SINGLETON_MULTI,
+};
+
+static ClassFactoryDict& _getMap(){
+	static  ClassFactoryDict _map;
+	return  _map;
+}
+
+template < class T, const char name[], int ClassType = DEFAULT> 
 class Register{
 public:
+	std::string GetClassName()const{return name_;}
 	static void *CreateInstance() {
-		return new T;
+		switch(ClassType){
+			case DEFAULT:
+				return new (std::nothrow) T;
+			case SINGLETON:
+				static T instance;
+				return &instance;
+			case SINGLETON_MULTI:
+			default:
+				return NULL;
+		};
 	}
 
-	static void *CreateSingletonInstance(){
-		static T instance;
-		return &T;
-	}
 public:
 	static const RegistyClass cRegist_;
-	static const RegistyClass cRegistSingleton_;
 protected:
-	Register() {} //this class only be used as base class
+	Register() : name_(name){
+		cRegist_.init();
+	} //this class only be used as base class
+	std::string name_;
 };
 
-template < class T, const char name[] >
+template < class T, const char name[], int ClassType>
 const RegistyClass
-Register < T, name >::cRegist_(name, Register < T, name >::CreateInstance);
-
-template < class T, const char name[] >
-const RegistyClass
-Register < T, name >::cRegist_(name, Register < T, name >::CreateSingletonInstance);
-
-#define DEFINE_CLASS(class_name) \
-	extern char class_name##Argv[];\
-	class class_name:public Register<class_name,class_name##Argv>
-
-#define DEFINE_CLASS_EX(class_name,father_class)\
-	extern char class_name##Argv[];\
-	class class_name:public Register<class_name,class_name##Argv>,public father_class
-
+Register<T, name, ClassType>::cRegist_(name, Register<T, name, ClassType>::CreateInstance);
 
 #endif
 
