7b6bcf46b12a7055b66918fac4af77584bc6d650
luyao <yaolu1103@gmail.com>
2014-03-08 14:00:30 +0800

    add license and some comments

--- include/class_factory.h
+++ include/class_factory.h
@@ -1,6 +1,43 @@
+/* 
+  Copyright (c) YaoLu(yaolu1103@gmail.com) All Rights Reserved
+  
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  * Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+  * Neither the name of the copyright holders nor the names of their
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 /** 
  * @file class_factory.h
  * @Synopsis  
+ *           this file provider some useful macro to define and declear the 
+ *           class;
+ *           User can only use: 
+ *           DECLEAR_CLASS
+ *           DECLEAR_SINGLETON_CLASS
+ *           DECLEAR_INTERFACE
+ *           DEFINE_CLASS
+ *           DECLEAR_CLASS_EX
+ *           DECLEAR_SINGLETON_CLASS_EX
+ *           and ClassFactory::GetClass(class_name)
+*             
  *
  * @author Yao Lu(yaolu1103@gmail.com) 
  * @version 1.0
